a20b7717bf63ac74b39e0ca13ae6e32abf8f8959
fdonnetTaff <fabien.donnet@ubiko.ch>
2023-10-04 17:01:55 +0200

    Begin of service

---
+++ src/Ubik.Accounting.Api/Service/ChartOfAccountsService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Ubik.Accounting.Api.Data;
+using Ubik.Accounting.Api.Models;
+
+namespace Ubik.Accounting.Api.Service
+{
+    public class ChartOfAccountsService
+    {
+        private readonly AccountingContext _context;
+        public ChartOfAccountsService(AccountingContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public async Task<IEnumerable<Account>> GetAccountsAsync()
+        {
+            return await _context.Accounts.ToListAsync();
+        }
+    }
+}
