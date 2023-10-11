77f5875cd21d68b9880954f3ac1de1543d79f250
donnetf@gmail.com <donnetf@gmail.com>
2023-10-11 21:00:13 +0200

    integration tests = not bad

--- src/Ubik.Accounting.Api.Test/Features/Accounts/AccountService_Test.cs
+++ src/Ubik.Accounting.Api.Test/Features/Accounts/AccountService_Test.cs
@@ -1,6 +1,7 @@
 ﻿using Bogus;
 using Bogus.Extensions;
 using FluentAssertions;
+using MediatR.NotificationPublishers;
 using Ubik.Accounting.Api.Data;
 using Ubik.Accounting.Api.Features;
 using Ubik.Accounting.Api.Features.Accounts.Exceptions;
@@ -26,7 +27,7 @@ namespace Ubik.Accounting.Api.Test.Features.Accounts
         public async Task GetAccountAsync_Success()
         {
             //Arrange
-            
+
             //Act
             var account = await _serviceManager.AccountService.GetAccountAsync(_testDBValues.AccountId1);
 
@@ -37,7 +38,7 @@ namespace Ubik.Accounting.Api.Test.Features.Accounts
         }
 
         [Theory, MemberData(nameof(GeneratedGuids))]
-        public async Task GetAccountAsync_Success_IsNull(Guid id)
+        public async Task GetAccountAsync_WhenIdNotExists_ThenReturnNull(Guid id)
         {
             //Arrange
 
@@ -62,7 +63,8 @@ namespace Ubik.Accounting.Api.Test.Features.Accounts
                     .And.Contain(x => x.Code == _testDBValues.AccountCode1);
         }
 
-        [Theory, MemberData(nameof(GeneratedAccounts))]
+        [Theory]
+        [MemberData(nameof(GetAccounts), parameters: new object[] { 5, "1524f11f-20dd-4888-88f8-428e59bbc22a"})]
         public async Task AddAccountAsync_Success(Account account)
         {
             //Arrange
@@ -72,8 +74,23 @@ namespace Ubik.Accounting.Api.Test.Features.Accounts
 
             //Assert
             account.Should()
-                    .NotBeNull();
+                    .NotBeNull()
+                    .And.BeOfType<Account>();
         }
+
+        [Theory]
+        [MemberData(nameof(GetAccounts), parameters: new object[] { 5, "1524f11f-20dd-4888-88f8-428e59bbc22b" })]
+        public async Task AddAccountAsync_WhenAccountGroupIdNotExists_ThenReturnException(Account account)
+        {
+            //Arrange
+
+            //Act
+            Func<Task> act = async () => await _serviceManager.AccountService.AddAccountAsync(account);
+
+            //Assert
+            await act.Should().ThrowAsync<Exception>();
+        }
+
 
         public static IEnumerable<object[]> GeneratedGuids
         {
@@ -85,43 +102,16 @@ namespace Ubik.Accounting.Api.Test.Features.Accounts
             }
         }
 
-        public static IEnumerable<object[]> GeneratedAccounts
+        public static IEnumerable<object[]> GetAccounts(int numTests, string accountGroupId)
         {
-            get
+            var accounts = FakeGenerator.GenerateAccounts(numTests, Guid.Parse(accountGroupId));
+
+            var data = new List<Object[]>();
+
+            foreach (var account in accounts)
             {
-                var account = new Faker<Account>("fr_CH")
-                .CustomInstantiator(a => new Account()
-                {
-                    Code = a.Lorem.Word().ClampLength(1, 20),
-                    Label = a.Lorem.Word().ClampLength(1, 100),
-                    Description = a.Lorem.Paragraph().ClampLength(1, 700),
-                    AccountGroupId = Guid.Parse("1524f11f-20dd-4888-88f8-428e59bbc22a")
-                });
-
-                var accounts = account.Generate(5);
-
-                for (int i = 1; i == 5; i++)
-                {
-                    yield return new object[] { accounts[i] };
-                }
+                yield return new object[] { account };
             }
         }
-
-        //KEEP FOR EXCPETION
-        //[Theory, MemberData(nameof(Guids))]
-        //public async Task GetAccountAsync_Failed_NotFound(Guid id)
-        //{
-        //    //Arrange
-
-        //    //Act
-        //    Func<Task> act = async () => await _serviceManager.AccountService.GetAccountAsync(id);
-
-        //    //Assert
-        //    await act.Should().ThrowAsync<AccountNotFoundException>().Where(x => x.ErrorType == ServiceAndFeatureExceptionType.NotFound);
-        //}
-
-
     }
-
-
 }
---
+++ src/Ubik.Accounting.Api.Test/Features/Accounts/FakeAccounts.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using Bogus.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ubik.Accounting.Api.Models;
+
+namespace Ubik.Accounting.Api.Test.Features.Accounts
+{
+    public static class FakeGenerator
+    {
+        public static IEnumerable<Account> GenerateAccounts(int numTests, Guid accountGroupId)
+        {
+            return new Faker<Account>("fr_CH")
+                 .CustomInstantiator(a => new Account()
+                 {
+                     Code = a.Finance.Account().ToString(),
+                     Label = a.Finance.AccountName().ClampLength(1, 100),
+                     Description = a.Lorem.Paragraphs().ClampLength(1, 700),
+                     AccountGroupId = accountGroupId
+                 }).Generate(numTests);
+        }
+    }
+}
