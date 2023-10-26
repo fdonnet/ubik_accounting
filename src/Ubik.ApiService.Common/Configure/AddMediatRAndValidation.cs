2c61b6c478eb7500d30e20612f93f85c8ead28f1
ubik <fabien.donnet@ubiko.ch>
2023-10-26 21:01:58 +0200

    comment

--- src/Ubik.ApiService.Common/Configure/AddMediatRAndValidation.cs
+++ src/Ubik.ApiService.Common/Configure/AddMediatRAndValidation.cs
@@ -19,6 +19,7 @@ namespace Ubik.ApiService.Common.Configure
     {
         public static void AddMediatRAndValidation(this IServiceCollection services, Assembly currentAssembly)
         {
+            //TODO: maybe we will reactivate mediatr validation pipeline...
             //services.AddValidatorsFromAssembly(currentAssembly);
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(currentAssembly));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
