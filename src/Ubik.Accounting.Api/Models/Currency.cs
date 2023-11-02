e428ada606114487cf1b02e11b8651af3cf86bfa
fdonnet <fabien.donnet@ubiko.ch>
2023-11-02 09:24:36 +0100

    remove currency audit fields because this entity will managed in other service

--- src/Ubik.Accounting.Api/Models/Currency.cs
+++ src/Ubik.Accounting.Api/Models/Currency.cs
@@ -4,16 +4,12 @@ using Ubik.DB.Common;
 namespace Ubik.Accounting.Api.Models
 {
     //TODO: will be updated by another service
-    public class Currency : ITenantEntity, IConcurrencyCheckEntity, IAuditEntity
+    public class Currency : ITenantEntity, IConcurrencyCheckEntity
     {
         public Guid Id { get; set; }
         [StringLength(3)]
         public required string IsoCode { get; set; }
         public Guid Version { get; set; }
         public Guid TenantId { get; set; }
-        public required DateTime CreatedAt { get; set; }
-        public required Guid CreatedBy { get; set; }
-        public DateTime? ModifiedAt { get; set; }
-        public Guid? ModifiedBy { get; set; }
     }
 }
