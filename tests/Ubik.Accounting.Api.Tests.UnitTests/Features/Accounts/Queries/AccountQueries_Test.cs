ec56cb73ab3d658c519bf0b4db869a92411d0d83
ubik <fabien.donnet@ubiko.ch>
2023-11-13 21:05:48 +0100

    Unit test for getAccountGroups for account (messsage bus)

--- tests/Ubik.Accounting.Api.Tests.UnitTests/Features/Accounts/Queries/AccountQueries_Test.cs
+++ tests/Ubik.Accounting.Api.Tests.UnitTests/Features/Accounts/Queries/AccountQueries_Test.cs
@@ -5,21 +5,23 @@ using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using Ubik.Accounting.Api.Features;
 using Ubik.Accounting.Api.Features.Accounts.Queries;
+using Ubik.Accounting.Api.Features.Accounts.Queries.CustomPoco;
 using Ubik.Accounting.Api.Models;
 using Ubik.Accounting.Contracts.Accounts.Queries;
 using Ubik.Accounting.Contracts.Accounts.Results;
 
 namespace Ubik.Accounting.Api.Tests.UnitTests.Features.Accounts.Queries
 {
-    public class GetAccount_Test : IAsyncLifetime
+    public class AccountQueries_Test : IAsyncLifetime
     {
         private readonly IServiceManager _serviceManager;
         private readonly GetAccountQuery _query;
+        private readonly GetAccountGroupsForAccountQuery _queryAccountGroups;
         private readonly Account _account;
         private ITestHarness _harness = default!;
         private IServiceProvider _provider = default!;
 
-        public GetAccount_Test()
+        public AccountQueries_Test()
         {
             _serviceManager = Substitute.For<IServiceManager>();
 
@@ -28,8 +30,17 @@ namespace Ubik.Accounting.Api.Tests.UnitTests.Features.Accounts.Queries
                 Id = Guid.NewGuid()
             };
 
+            _queryAccountGroups = new GetAccountGroupsForAccountQuery()
+            {
+                AccountId = Guid.NewGuid()
+            };
+
             _account = new Account() { Code = "TEST", Label = "Test", CurrencyId = Guid.NewGuid() };
             _serviceManager.AccountService.GetAsync(_query.Id).Returns(_account);
+            _serviceManager.AccountService.GetAccountGroupsAsync(_queryAccountGroups.AccountId).Returns(new List<AccountGroupClassification>
+            {
+                new AccountGroupClassification{ClassificationCode="TEst",ClassificationId=Guid.NewGuid()},
+            });
         }
 
         public async Task InitializeAsync()
@@ -39,6 +50,7 @@ namespace Ubik.Accounting.Api.Tests.UnitTests.Features.Accounts.Queries
                 {
                     x.AddScoped<IServiceManager>(sm => _serviceManager);
                     x.AddConsumer<GetAccountConsumer>();
+                    x.AddConsumer<GetAccountGroupsForAccountConsumer>();
 
                 }).BuildServiceProvider(true);
 
@@ -67,6 +79,29 @@ namespace Ubik.Accounting.Api.Tests.UnitTests.Features.Accounts.Queries
             response.Message.Should()
                 .BeOfType<GetAccountResult>()
                 .And.Match<GetAccountResult>(a => a.Code == _account.Code);
+        }
+
+        [Fact]
+        public async Task GetAccountGroups_AccountGroupsClassification_Ok()
+        {
+            //Arrange
+            var client = _harness.GetRequestClient<GetAccountGroupsForAccountQuery>();
+            var consumerHarness = _harness.GetConsumerHarness<GetAccountGroupsForAccountConsumer>();
+
+            //Act
+            var response = await client.GetResponse<GetAccountGroupClassificationResults>(_queryAccountGroups);
+
+            //Assert
+            var sent = await _harness.Sent.Any<GetAccountGroupClassificationResults>();
+            var consumed = await _harness.Consumed.Any<GetAccountGroupsForAccountQuery>();
+            var consumerConsumed = await consumerHarness.Consumed.Any<GetAccountGroupsForAccountQuery>();
+
+            sent.Should().Be(true);
+            consumed.Should().Be(true);
+            consumerConsumed.Should().Be(true);
+            response.Message.Should()
+                .BeOfType<GetAccountGroupClassificationResults>()
+                .And.Match<GetAccountGroupClassificationResults>(a => a.AccountGroups.Any());
         }
 
         public async Task DisposeAsync()
