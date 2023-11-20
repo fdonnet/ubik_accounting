4f681d8f2a0493da1fde40ba82bd5bd5bbca8297
ubik <fabien.donnet@ubiko.ch>
2023-11-20 21:34:00 +0100

    auth shit, i hate that

--- src/Ubik.Accounting.WebApp/Program.cs
+++ src/Ubik.Accounting.WebApp/Program.cs
@@ -9,12 +9,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Ubik.Accounting.WebApp.ApiClients;
 using Ubik.Accounting.WebApp.Components;
 using Ubik.Accounting.WebApp.Security;
 using Ubik.ApiService.Common.Configure.Options;
+using System.Net.Http;
 using static Ubik.Accounting.WebApp.Security.UserService;
+using IdentityModel.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,11 +38,66 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 
 builder.Services.AddAuthentication(options =>
 {
+    //options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
-    .AddCookie()
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+        //options.Events = new CookieAuthenticationEvents
+        //{
+        //    // this event is fired everytime the cookie has been validated by the cookie middleware,
+        //    // so basically during every authenticated request
+        //    // the decryption of the cookie has already happened so we have access to the user claims
+        //    // and cookie properties - expiration, etc..
+        //    OnValidatePrincipal = async x =>
+        //    {
+        //        // since our cookie lifetime is based on the access token one,
+        //        // check if we're more than halfway of the cookie lifetime
+        //        var now = DateTimeOffset.UtcNow;
+        //        var timeElapsed = now.Subtract(x.Properties.IssuedUtc.Value);
+        //        var timeRemaining = x.Properties.ExpiresUtc.Value.Subtract(now);
+
+        //        if (timeElapsed > timeRemaining)
+        //        {
+        //            var identity = (ClaimsIdentity)x.Principal.Identity;
+        //            var accessTokenClaim = identity.FindFirst("access_token");
+        //            var refreshTokenClaim = identity.FindFirst("refresh_token");
+
+        //            // if we have to refresh, grab the refresh token from the claims, and request
+        //            // new access token and refresh token
+        //            var refreshToken = refreshTokenClaim.Value;
+        //            var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
+        //            {
+        //                Address = authOptions.Authority,
+        //                ClientId = "ubik_accounting_clientapp",
+        //                ClientSecret = "ZHwmuIwjkdwTPeTZqfAst1YxiY27FgZq",
+        //                RefreshToken = refreshToken
+        //            });
+
+        //            if (!response.IsError)
+        //            {
+        //                // everything went right, remove old tokens and add new ones
+        //                identity.RemoveClaim(accessTokenClaim);
+        //                identity.RemoveClaim(refreshTokenClaim);
+
+        //                identity.AddClaims(new[]
+        //                {
+        //                                new Claim("access_token", response.AccessToken),
+        //                                new Claim("refresh_token", response.RefreshToken)
+        //                            });
+
+        //                // indicate to the cookie middleware to renew the session cookie
+        //                // the new lifetime will be the same as the old one, so the alignment
+        //                // between cookie and access token is preserved
+        //                x.ShouldRenew = true;
+        //            }
+        //        }
+        //    }
+        //};
+    })
     .AddOpenIdConnect(options =>
     {
         {
@@ -62,15 +120,34 @@ builder.Services.AddAuthentication(options =>
                 NameClaimType = "name",
             };
 
-            options.CallbackPath = new PathString("/callback");
+            //options.Events = new OpenIdConnectEvents
+            //{
+            //    // that event is called after the OIDC middleware received the auhorisation code,
+            //    // redeemed it for an access token and a refresh token,
+            //    // and validated the identity token
+            //    OnTokenValidated = x =>
+            //    {
+            //        // store both access and refresh token in the claims - hence in the cookie
+            //        var identity = (ClaimsIdentity)x.Principal.Identity;
+            //        identity.AddClaims(new[]
+            //        {
+            //                    new Claim("access_token", x.TokenEndpointResponse.AccessToken),
+            //                    new Claim("refresh_token", x.TokenEndpointResponse.RefreshToken)
+            //                });
 
-            options.Events = new OpenIdConnectEvents
-            {
-                OnRedirectToIdentityProvider = context => {
-                    context.ProtocolMessage.SetParameter("audience", "https://my/api");
-                    return Task.CompletedTask;
-                }
-            };
+            //        // so that we don't issue a session cookie but one with a fixed expiration
+            //        x.Properties.IsPersistent = true;
+
+            //        // align expiration of the cookie with expiration of the
+            //        // access token
+            //        var accessToken = new JwtSecurityToken(x.TokenEndpointResponse.AccessToken);
+            //        x.Properties.ExpiresUtc = accessToken.ValidTo;
+
+            //        return Task.CompletedTask;
+            //    }
+            //};
+
+            options.CallbackPath = new PathString("/callback");
         }
     });
 
