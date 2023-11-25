4287268ebf8d0eeacbdcf6a5901f623397d32ccd
ubik <fabien.donnet@ubiko.ch>
2023-11-25 20:04:04 +0100

    tailwind start

---
+++ src/Ubik.Accounting.WebApp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.razor'
+  ],
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
