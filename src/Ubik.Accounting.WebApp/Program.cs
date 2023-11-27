cecde2630457994787e921f2e4141d459bf0dd32
ubik <fabien.donnet@ubiko.ch>
2023-11-27 21:07:33 +0100

    first working version with theme, need to go with apply etc

--- src/Ubik.Accounting.WebApp/Program.cs
+++ src/Ubik.Accounting.WebApp/Program.cs
@@ -186,7 +186,7 @@ app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Auth).Assembly);
+    .AddAdditionalAssemblies(typeof(ManageAccounts).Assembly);
 //.AddAdditionalAssemblies(typeof(AccoutsList).Assembly)
 //.AddAdditionalAssemblies(typeof(Auth).Assembly);
 
