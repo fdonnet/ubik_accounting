3309da241625ed87f5a451f328ecf9ae170d3c78
ubik <fabien.donnet@ubiko.ch>
2023-11-28 21:25:17 +0100

    update tailwind

--- src/Ubik.Accounting.WebApp/tailwind.config.js
+++ src/Ubik.Accounting.WebApp/tailwind.config.js
@@ -6,13 +6,35 @@ module.exports = {
       'Inter',
       'ui-sans-serif',
       'system-ui',
-      // other fallback fonts
+      '-apple-system',
+      'system-ui',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'Noto Sans',
+      'sans-serif',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      'Noto Color Emoji'
     ],
     'sans': [
       'Inter',
       'ui-sans-serif',
       'system-ui',
-      // other fallback fonts
+      '-apple-system',
+      'system-ui',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'Noto Sans',
+      'sans-serif',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      'Noto Color Emoji'
     ]
   },
   content: [
