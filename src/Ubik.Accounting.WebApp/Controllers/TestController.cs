755c813119ffc9cb1c8b9f5ddcc1a04bb11bcf9b
ubik <fabien.donnet@ubiko.ch>
2023-11-28 14:43:53 +0100

    working version but flcikering on wasm client

--- src/Ubik.Accounting.WebApp/Controllers/TestController.cs
+++ src/Ubik.Accounting.WebApp/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Ubik.Accounting.WebApp.ApiClients;
 using Ubik.Accounting.Contracts.Accounts.Results;
 using Ubik.Accounting.Webapp.Shared.Facades;
+using System.Net.Http;
 
 namespace Ubik.Accounting.WebApp.Controllers
 {
@@ -20,9 +21,24 @@ namespace Ubik.Accounting.WebApp.Controllers
         }
 
         [HttpGet("/GetAllAccounts")]
-        public async Task<HttpResponseMessage> AccountList()
+        public async Task<ActionResult<IEnumerable<GetAllAccountsResult>>> AccountList()
         {
-            return await client.GetAllAccountsAsync();
+            var response = await client.GetAllAccountsAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<IEnumerable<GetAllAccountsResult>>();
+                return Ok(result);
+            }
+            else
+            {
+                //TODO: write error handling here with problem details etc (onyl forward it to UI)
+                //in case of big error throw an exception or send a problem detail back need to be seen.
+                return new ObjectResult("ERROR")
+                {
+                    StatusCode = (int?)response.StatusCode,
+                };
+            }
         }
 
     }
