c0ba735eb22b8b0f0ad66209d9133a9137d72f0f
ubik <fabien.donnet@ubiko.ch>
2023-11-29 23:19:47 +0100

    modal click outisde

--- src/Ubik.Accounting.WebApp.Client/Components/Accounts/ModalAccountAddEdit.razor.js
+++ src/Ubik.Accounting.WebApp.Client/Components/Accounts/ModalAccountAddEdit.razor.js
@@ -5,3 +5,17 @@ export function openDialog(dialogId) {
 export function closeDialog(dialogId) {
   document.getElementById(dialogId).close();
 }
+
+export function clickOutside(dialogId) {
+  var dialog = document.getElementById(dialogId);
+
+  dialog.addEventListener('click', (event) => {
+    if (event.target.id !== 'inside-modal') {
+      dialog.close();
+    }
+  });
+}
+
+
+
+
