b81aa5eae92ef6cb74d2715521de3fe38ab92cbb
ubik <fabien.donnet@ubiko.ch>
2023-12-04 12:09:41 +0100

    super select component

---
+++ src/Ubik.Accounting.WebApp.Client/Components/Common/Forms/UbikCSSConst.cs
@@ -0,0 +1,10 @@
+﻿namespace Ubik.Accounting.WebApp.Client.Components.Common.Forms
+{
+    public static class UbikCSSConst
+    {
+        public const string VALID_LABEL_CSS_CLASS = "block mb-2 text-sm font-medium text-gray-900 dark:text-white";
+        public const string INVALID_LABEL_CSS_CLASS = "block mb-2 text-sm font-medium text-red-700 dark:text-red-500";
+        public const string VALID_SELECT_CSS_CLASS = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+        public const string INVALID_SELECT_CSS_CLASS = "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500";
+    }
+}
