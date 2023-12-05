c34d8c2e20cd5775d0cdced0028486b3c5ea66c6
ubik <fabien.donnet@ubiko.ch>
2023-12-05 15:34:20 +0100

    before old view grid removal

--- src/Ubik.Accounting.WebApp.Client/Components/Common/Grid/UbikGrid.razor.cs
+++ src/Ubik.Accounting.WebApp.Client/Components/Common/Grid/UbikGrid.razor.cs
@@ -5,6 +5,28 @@ namespace Ubik.Accounting.WebApp.Client.Components.Common.Grid
     public partial class UbikGrid<TGridItem>
     {
         [Parameter] public IQueryable<TGridItem>? Items { get; set; }
-        [Parameter] public List<string>? FieldNames { get; set; } = null;
+        [Parameter] public List<string> FieldNames { get; set; } = null;
+        [Parameter] public bool EditAndRemoveButton { get; set; } = false;
+
+        private readonly RenderFragment _renderLoading;
+        private readonly RenderFragment _renderColumnHeaders;
+        private readonly RenderFragment _renderRows;
+        private readonly RenderFragment _renderEditAndRemoveButtons;
+
+        private int _columnNumber = 0;
+
+        public UbikGrid()
+        {
+            _renderLoading = RenderLoading;
+            _renderColumnHeaders = RenderColumnHeaders;
+            _renderRows = RenderRows;
+            _renderEditAndRemoveButtons = RenderEditRemoveButton;
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            _columnNumber = EditAndRemoveButton ? FieldNames.Count + 2 : FieldNames.Count; 
+            return base.OnInitializedAsync();
+        }
     }
 }
