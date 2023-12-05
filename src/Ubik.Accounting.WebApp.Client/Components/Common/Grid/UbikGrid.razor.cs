781e7f30dfc7ee8a74a3bdfd456231a18326a590
ubik <fabien.donnet@ubiko.ch>
2023-12-05 15:13:54 +0100

    Ok grid EZ

---
+++ src/Ubik.Accounting.WebApp.Client/Components/Common/Grid/UbikGrid.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Ubik.Accounting.WebApp.Client.Components.Common.Grid
+{
+    public partial class UbikGrid<TGridItem>
+    {
+        [Parameter] public IQueryable<TGridItem>? Items { get; set; }
+        [Parameter] public List<string>? FieldNames { get; set; } = null;
+    }
+}
