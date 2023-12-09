d818b94f06e4b7ce45857cd139a2126a708b3c73
ubik <fabien.donnet@ubiko.ch>
2023-12-09 15:38:56 +0100

    First version of validation error simple msg

---
+++ src/Ubik.Accounting.WebApp.Client/Components/Common/Errors/ErrCannotSumbit.razor.js
@@ -0,0 +1,11 @@
+export function openErrorSubmit(alertId) {
+  var panel = document.getElementById(alertId);
+  panel.classList.remove("hidden");
+  panel.classList.add("block");
+}
+
+export function closeErrorSubmit(alertId) {
+  var panel = document.getElementById(alertId);
+  panel.classList.remove("block");
+  panel.classList.add("hidden");
+}
