69224be84881245ff65835dfe40ac55a3264df24
ubik <fabien.donnet@ubiko.ch>
2024-11-02 20:45:43 +0100

    Test fro TaxRates

--- tests/Ubik.Api.Tests.Integration/Features/Accounting/SalesOrVatTax/TaxRates/TaxRatesController_Test.cs
+++ tests/Ubik.Api.Tests.Integration/Features/Accounting/SalesOrVatTax/TaxRates/TaxRatesController_Test.cs
@@ -38,6 +38,7 @@ namespace Ubik.Api.Tests.Integration.Features.Accounting.SalesOrVatTax.TaxRates
 
             //Act
             var response = await _client.GetAsync(_baseUrlForV1);
+            var tmp = await response.Content.ReadAsStringAsync();
             var result = await response.Content.ReadFromJsonAsync<List<SalesOrVatTaxRateStandardResult>>();
 
             //Assert
@@ -263,6 +264,35 @@ namespace Ubik.Api.Tests.Integration.Features.Accounting.SalesOrVatTax.TaxRates
         }
 
         [Fact]
+        public async Task Add_TaxRate_WithAlreadyExists_409()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new AddSalesOrVatTaxRateCommand
+            {
+                Code = "v81",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync(_baseUrlForV1, command);
+            var tmp = await response.Content.ReadAsStringAsync();
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            result.Should()
+            .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "TAXRATE_ALREADY_EXISTS");
+        }
+
+        [Fact]
         public async Task Add_TaxRate_WithRO_403()
         {
             //Arrange
@@ -379,5 +409,349 @@ namespace Ubik.Api.Tests.Integration.Features.Accounting.SalesOrVatTax.TaxRates
             response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
 
+        [Fact]
+        public async Task Update_TaxRate_WithRW_OK()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+            var tmp = await response.Content.ReadAsStringAsync();
+            var result = await response.Content.ReadFromJsonAsync<SalesOrVatTaxRateStandardResult>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<SalesOrVatTaxRateStandardResult>()
+                .And.Match<SalesOrVatTaxRateStandardResult>(x => x.Code == command.Code);
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithRO_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RO);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithNoToken_401()
+        {
+            //Arrange
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithAdmin_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.MegaAdmin);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithOtherTenant_404()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.OtherTenant);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "TAXRATE_NOT_FOUND");
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithNoRole_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.NoRole);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "Test2",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+
+
+        [Fact]
+        public async Task Update_TaxRate_WithNotMatchId_400()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = NewId.NextGuid(),
+                Code = "Test3",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "TAXRATE_UPDATE_IDS_NOT_MATCH");
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithAlreadyExists_409()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = _idToUpd,
+                Code = "v81",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{_idToUpd}", command);
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "TAXRATE_ALREADY_EXISTS");
+        }
+
+        [Fact]
+        public async Task Update_TaxRate_WithBadId_404()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var id = NewId.NextGuid();
+            var command = new UpdateSalesOrVatTaxRateCommand
+            {
+                Id = id,
+                Code = "v81",
+                Description = "Description",
+                Rate = 7.4m,
+                ValidFrom = DateOnly.FromDateTime(DateTime.Now),
+                ValidTo = null,
+                Version = new Guid("e4220000-3c36-7456-088c-08dcfb4a4b66")
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"{_baseUrlForV1}/{id}", command);
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "TAXRATE_NOT_FOUND");
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithRW_OK()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithRO_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RO);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithNoToken_401()
+        {
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithAdmin_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.MegaAdmin);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithOtherTenant_404()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.OtherTenant);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithNoRole_403()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.NoRole);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{_idToDel}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Delete_TaxRate_WithBadId_404()
+        {
+            //Arrange
+            var token = await GetAccessTokenAsync(TokenType.RW);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            //Act
+            var response = await _client.DeleteAsync($"{_baseUrlForV1}/{NewId.NextGuid()}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
     }
 }
