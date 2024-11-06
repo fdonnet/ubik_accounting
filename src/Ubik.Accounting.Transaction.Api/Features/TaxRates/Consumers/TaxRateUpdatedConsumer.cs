15060cdfafb6ec9c25782e6701dc43e52e65e1e2
fdonnet <fdonnet@test.com>
2024-11-06 17:55:44 +0100

    Consumer for tax rates in transaction API

---
+++ src/Ubik.Accounting.Transaction.Api/Features/TaxRates/Consumers/TaxRateUpdatedConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using Ubik.Accounting.SalesOrVatTax.Contracts.TaxRates.Events;
+using Ubik.Accounting.Structure.Contracts.Accounts.Events;
+using Ubik.Accounting.Transaction.Api.Features.TaxRates.Services;
+
+namespace Ubik.Accounting.Transaction.Api.Features.TaxRates.Consumers
+{
+    public class TaxRateUpdatedConsumer(ITaxRateCommandService commandService) : IConsumer<TaxRateUpdated>
+    {
+        public async Task Consume(ConsumeContext<TaxRateUpdated> context)
+        {
+            await commandService.UpdateAsync(context.Message);
+        }
+    }
+}
+
