ecdd453dd20ac143a445b8bef9b42044b3871aae
ubik <fabien.donnet@ubiko.ch>
2024-11-08 16:43:43 +0100

    Base routes testing for submit TX (security)

--- tests/Ubik.Api.Tests.Integration/Features/Accounting/Transaction/Txs/TxsController_Test.cs
+++ tests/Ubik.Api.Tests.Integration/Features/Accounting/Transaction/Txs/TxsController_Test.cs
@@ -12,6 +12,7 @@ using Ubik.Accounting.Transaction.Contracts.Txs.Commands;
 using Ubik.Accounting.Transaction.Contracts.Txs.Events;
 using FluentAssertions;
 using Ubik.Accounting.Transaction.Contracts.Entries.Enums;
+using Ubik.ApiService.Common.Exceptions;
 
 namespace Ubik.Api.Tests.Integration.Features.Accounting.Transaction.Txs
 {
@@ -74,6 +75,228 @@ namespace Ubik.Api.Tests.Integration.Features.Accounting.Transaction.Txs
             .NotBeNull()
                 .And.BeOfType<TxSubmited>()
                 .And.Match<TxSubmited>(x => x.Amount == command.Amount);
+        }
+
+        [Fact]
+        public async Task Submit_Tx_WithRO_403()
+        {
+            var token = await GetAccessTokenAsync(TokenType.RO);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new SubmitTxCommand()
+            {
+                Amount = 1000,
+                ValueDate = DateOnly.FromDateTime(DateTime.Now),
+                Entries = new List<SubmitTxEntry>()
+                {
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-c1ce-08dcd98b7c74"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Debit,
+                        TaxInfo = null,
+                        Type = EntryType.Main
+                    },
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-48fb-08dcd98b4a28"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Credit,
+                        TaxInfo = null,
+                        Type = EntryType.Counterparty
+                    }
+                }
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"{_baseUrlForV1}/txs/submit", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Submit_Tx_WithNoToken_401()
+        {
+            var command = new SubmitTxCommand()
+            {
+                Amount = 1000,
+                ValueDate = DateOnly.FromDateTime(DateTime.Now),
+                Entries = new List<SubmitTxEntry>()
+                {
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-c1ce-08dcd98b7c74"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Debit,
+                        TaxInfo = null,
+                        Type = EntryType.Main
+                    },
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-48fb-08dcd98b4a28"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Credit,
+                        TaxInfo = null,
+                        Type = EntryType.Counterparty
+                    }
+                }
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"{_baseUrlForV1}/txs/submit", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Submit_Tx_WithAdmin_403()
+        {
+            var token = await GetAccessTokenAsync(TokenType.MegaAdmin);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new SubmitTxCommand()
+            {
+                Amount = 1000,
+                ValueDate = DateOnly.FromDateTime(DateTime.Now),
+                Entries = new List<SubmitTxEntry>()
+                {
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-c1ce-08dcd98b7c74"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Debit,
+                        TaxInfo = null,
+                        Type = EntryType.Main
+                    },
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-48fb-08dcd98b4a28"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Credit,
+                        TaxInfo = null,
+                        Type = EntryType.Counterparty
+                    }
+                }
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"{_baseUrlForV1}/txs/submit", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Submit_Tx_WithNoRole_403()
+        {
+            var token = await GetAccessTokenAsync(TokenType.NoRole);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new SubmitTxCommand()
+            {
+                Amount = 1000,
+                ValueDate = DateOnly.FromDateTime(DateTime.Now),
+                Entries = new List<SubmitTxEntry>()
+                {
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-c1ce-08dcd98b7c74"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Debit,
+                        TaxInfo = null,
+                        Type = EntryType.Main
+                    },
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-48fb-08dcd98b4a28"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Credit,
+                        TaxInfo = null,
+                        Type = EntryType.Counterparty
+                    }
+                }
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"{_baseUrlForV1}/txs/submit", command);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Submit_Tx_WithOtherTenant_400()
+        {
+            var token = await GetAccessTokenAsync(TokenType.OtherTenant);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var command = new SubmitTxCommand()
+            {
+                Amount = 1000,
+                ValueDate = DateOnly.FromDateTime(DateTime.Now),
+                Entries = new List<SubmitTxEntry>()
+                {
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-c1ce-08dcd98b7c74"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Debit,
+                        TaxInfo = null,
+                        Type = EntryType.Main
+                    },
+                    new SubmitTxEntry()
+                    {
+                        AccountId = new Guid("248e0000-5dd4-0015-48fb-08dcd98b4a28"),
+                        Amount = 1000,
+                        AmountAdditionnalInfo = null,
+                        Description = "Test",
+                        Label = "Test",
+                        Sign = DebitCredit.Credit,
+                        TaxInfo = null,
+                        Type = EntryType.Counterparty
+                    }
+                }
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"{_baseUrlForV1}/txs/submit", command);
+            var result = await response.Content.ReadFromJsonAsync<CustomProblemDetails>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            result.Should()
+            .NotBeNull()
+                .And.BeOfType<CustomProblemDetails>()
+                .And.Match<CustomProblemDetails>(x => x.Errors.First().Code == "ACCOUNT_NOT_FOUND_FOR_ENTRY");
         }
     }
 }
