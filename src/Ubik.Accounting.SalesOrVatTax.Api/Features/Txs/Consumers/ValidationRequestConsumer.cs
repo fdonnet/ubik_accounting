d5ebfe4d66b55b68de0a5d40c72e3034ea5423d4
fdonnet <fdonnet@test.com>
2024-11-13 14:09:44 +0100

    Fake consumer for tax validation

---
+++ src/Ubik.Accounting.SalesOrVatTax.Api/Features/Txs/Consumers/ValidationRequestConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Ubik.Accounting.Transaction.Contracts.Txs.Events;
+
+namespace Ubik.Accounting.SalesOrVatTax.Api.Features.Txs.Consumers
+{
+    public class ValidationRequestConsumer(IPublishEndpoint publishEndpoint) : IConsumer<TxTaxValidationRequestSent>
+    {
+        //TODO: Publish and validation need to be put in a service
+        public async Task Consume(ConsumeContext<TxTaxValidationRequestSent> context)
+        {
+            var tx = context.Message.Tx;
+            var random = new Random();
+            var i = random.Next(2);
+
+            if(i == 0)
+            {
+                await publishEndpoint.Publish(new TxValidated
+                {
+                    Id = tx.Id,
+                    Version = tx.Version
+                });
+            }
+            else
+            {
+                await publishEndpoint.Publish(new TxRejected
+                {
+                    Id = tx.Id,
+                    Version = tx.Version,
+                    Reason = "Random rejection"
+                });
+            }
+        }
+    }
+}
---
+++ src/Ubik.Accounting.Transaction.Contracts/Txs/Events/TxRejected.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubik.Accounting.Transaction.Contracts.Txs.Events
+{
+    public class TxRejected
+    {
+        public required Guid Id { get; init; }
+        public required Guid Version { get; init; }
+        public required string Reason { get; init; }
+    }
+}
